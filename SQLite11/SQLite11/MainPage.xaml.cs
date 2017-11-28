f68bd2a3880f0a1a45b5de1a3a4759ec1df93e55
裕登 鎌田 <150265@jc-21.jp>
2017-11-28 12:46:46 +0900

    button

--- SQLite11/SQLite11/MainPage.xaml.cs
+++ SQLite11/SQLite11/MainPage.xaml.cs
@@ -6,6 +6,7 @@ namespace SQLite11
 {
     public partial class MainPage : ContentPage
     {
+        /*
         public MainPage()
         {
             InitializeComponent();
@@ -29,6 +30,38 @@ namespace SQLite11
 
             Content = layout;
 
+        }*/
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var layout = new StackLayout { HorizontalOptions = LayoutOptions.Center, Margin = new Thickness { Top = 100 } };
+
+            //Userテーブルに適当なデータを追加する
+            UserModel.insertUser("鈴木");
+            UserModel.insertUser("田中");
+            UserModel.insertUser("斎藤");
+
+            var buttonAdd = new Button
+            {
+                WidthRequest = 60,
+                TextColor = Color.White,
+                Text = "Add"
+            };
+
+            layout.Children.Add(buttonAdd);
+
+            //Userテーブルの行データを取得
+            var query = UserModel.selectUser();
+
+            foreach (var user in query)
+            {
+
+                //Userテーブルの名前列をLabelに書き出す
+                layout.Children.Add(new Label { Text = user.Name });
+            }
+
+            Content = layout;
         }
     }
 }
